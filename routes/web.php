4db6af90d7d73a25a8212fe0b5732b5f64cd90b6
AliSulaimanLee <2nd.alisulaiman@gmail.com>
2023-05-16 15:21:39 +0700

    s

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test-db-connection', function () {
+    try {
+        DB::connection()->getPdo();
+        echo "Connection successful.";
+    } catch (\Exception $e) {
+        die("Could not connect to the database. " . $e->getMessage());
+    }
+});
+
